74ab4a9c8fb17dd9721b59ddaf5bf8db0e919a08
qwang1990 <526186564@qq.com>
2016-08-08 22:41:20 +0800

    ok

--- act/src/main/java/com/activity/controller/MyController.java
+++ act/src/main/java/com/activity/controller/MyController.java
@@ -1,14 +1,33 @@
 package com.activity.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.activiti.bpmn.model.BpmnModel;
+import org.activiti.engine.ManagementService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.RepositoryServiceImpl;
+import org.activiti.engine.impl.cmd.GetDeploymentProcessDiagramCmd;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.runtime.Execution;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.activiti.image.ProcessDiagramGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +42,16 @@ public class MyController {
 	
 	 @Autowired
 	 private TaskService taskService;
+	 
+	 @Autowired
+	 private ManagementService managementService;
+	 
+	 @Autowired
+	 private RepositoryService repositoryService;
+	 
+	 @Autowired
+	 private ProcessEngine processEngine;
+
 	 
 	 @RequestMapping("/")
 		public String index() {
@@ -41,25 +70,54 @@ public class MyController {
 	
 	@RequestMapping("/process")
 	public String managerInterview(@RequestParam("role") String username,Map<String, Object> mode) {
-	   List<Task> tasks = taskService.createTaskQuery().taskAssignee(username).list();
-	   
-	   if(tasks.size()==0){
-		   System.out.println("您没有需要处理的任务");
-		   return "error";
-	   }
+	    List<Task> tasksAssignee = taskService.createTaskQuery().taskAssignee(username).list();
+		List<Task> tasksCandidateUser = taskService.createTaskQuery().taskCandidateUser(username).list();
+//	   if(tasksAssignee.size()==0){
+//		   System.out.println("您没有需要处理的任务");
+//		   return "error";
+//	   }
 	   
 	   List<TaskMess> mess = new ArrayList();
 	   
-	   for(Task t:tasks){
+	   for(Task t:tasksAssignee){
 		   Execution exe = runtimeService.createExecutionQuery().executionId(t.getExecutionId()).singleResult();
+		   String processId = exe.getProcessInstanceId();
+		  
+		   //String processId = t.getProcessDefinitionId();
+		   
+		   
+
 		 //  System.out.println(username+"通过"+runtimeService.getVariable(exe.getId(), "username"));
 		   TaskMess temp = new TaskMess();
 		   temp.setTask(t);
 		   temp.setUsername((String)runtimeService.getVariable(exe.getId(), "username"));
+		   temp.setProcessId(processId);
 		   mess.add(temp);
 		   //taskService.complete(t.getId());
+		   
 	   }
+	   
+	   
+	   List<TaskMess> mess2 = new ArrayList();
+	   
+	   for(Task t:tasksCandidateUser){
+		   Execution exe = runtimeService.createExecutionQuery().executionId(t.getExecutionId()).singleResult();
+		   String processId = exe.getProcessInstanceId();
+		 //  System.out.println(username+"通过"+runtimeService.getVariable(exe.getId(), "username"));
+		   TaskMess temp = new TaskMess();
+		   temp.setTask(t);
+		   temp.setUsername((String)runtimeService.getVariable(exe.getId(), "username"));
+		   temp.setProcessId(processId);
+		   mess2.add(temp);
+		   //taskService.complete(t.getId());
+	   }
+	   
+	   
+	   
+	   
+	   
 	   mode.put("tasks", mess);
+	   mode.put("tasks2", mess2);
 	   return "success";
 	}
 	
@@ -72,14 +130,92 @@ public class MyController {
         return "ok";
 	}
 	
+	@RequestMapping("/claim/{id}/{exeid}")
+	public String claimApply(@PathVariable("id") String id,@PathVariable("exeid") String exeid,Map<String, Object> mode) {
+	//public String apply(@RequestParam("task") Task task,Map<String, Object> mode) {
+		Execution exe = runtimeService.createExecutionQuery().executionId(exeid).singleResult();
+		System.out.println("claim"+runtimeService.getVariable(exe.getId(), "username"));
+		taskService.claim(id, "hr");
+        return "ok";
+	}
 	
+	@RequestMapping("/image/{processId}")
+	public void gene(HttpServletRequest request,HttpServletResponse response,@PathVariable("processId") String processId){
+		 FileInputStream fis = null;
+		    response.setContentType("image/gif");
+		    try {
+		    	OutputStream out = response.getOutputStream();
+		    	//Command cmd=null;
+				//这里输入的流程ID，不是KEY
+				//cmd=new GetDeploymentProcessDiagramCmd(processId);
+		    	ProcessInstance pi = runtimeService.createProcessInstanceQuery()
+		    			.processInstanceId(processId).singleResult();
+		    	
+		    	RepositoryServiceImpl rs = (RepositoryServiceImpl)processEngine.getRepositoryService();
+		    	
+		    	ProcessDefinitionEntity pd = (ProcessDefinitionEntity) rs.getDeployedProcessDefinition(pi.getProcessDefinitionId());
+		    	 BpmnModel bpmnModel =
+		    			 repositoryService.getBpmnModel(pi.getProcessDefinitionId());
+		    	 
+		    	 				 //ProcessDiagramGenerator.generateDiagram
+		    	 InputStream is = processEngine.getProcessEngineConfiguration().getProcessDiagramGenerator()
+		    	 .generateDiagram(bpmnModel, "png", runtimeService.getActiveActivityIds(pi.getId()));
+		    	//InputStream is = ProcessDiagramGenerator.generateDiagram(pd,"png",
+		    		//	runtimeService.getActiveActivityIds(processId));
+				ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+		    	int len = 0;
+		    	while((len=is.read())!=-1){
+		    		bytestream.write(len);
+		    	}
+				byte[] b = bytestream.toByteArray();
+				bytestream.close();
+				out.write(b);
+				
+//				while ((len = is.read(b, 0, 1024)) != -1) {
+//					System.out.write(b, 0, len);
+//					out.write(b);
+//					}
+				out.flush();
+		    	
+		    	
+		    	
+		        
+//		        File file = new File("timg.jpg");
+//		        fis = new FileInputStream(file);
+//		        byte[] b = new byte[fis.available()];
+//		        fis.read(b);
+//		        out.write(b);
+//		        out.flush();
+		    } catch (Exception e) {
+		         e.printStackTrace();
+		    } finally {
+		        if (fis != null) {
+		            try {
+		               fis.close();
+		            } catch (IOException e) {
+			        e.printStackTrace();
+			    }   
+		          }
+		    }
+	}
+
 }
 
 class TaskMess{
 	private Task task;
 	private String username;
+	private String processId;
+
 	
 	public TaskMess(){
+	}
+
+	public void setProcessId(String processId){
+		this.processId = processId;
+	}
+	
+	public String getProcessId(){
+		return processId;
 	}
 	
 	public void setTask(Task task){
